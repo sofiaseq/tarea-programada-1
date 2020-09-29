1d4de62f5aa213904b43b21647ed94ae88c4bbbf
sofiaseq <sofia.sequeiraugalde@ucr.ac.cr>
2020-09-29 01:10:52 -0600

    Update Cementerio

--- Cementerio.java
+++ Cementerio.java
@@ -16,4 +16,37 @@ public class Cementerio{
     }
     return ultimaCarta;
    }
+   
+   public String ultimaCartaColor(Carta[] baraja){
+        String colorUltimaCarta = "";
+        for(int i = 0; i < baraja.length; i++){
+            if(baraja[i] == null){
+            colorUltimaCarta = baraja[i-1].getColor();
+            break;
+        }
+       }
+         return colorUltimaCarta;
+   }
+   
+   public int ultimaCartaSimbolo(Carta[] baraja){
+        int simboloUltimaCarta = 0;
+        for(int i = 0; i < baraja.length; i++){
+            if(baraja[i] == null){
+            simboloUltimaCarta = baraja[i-1].getSimbolo();
+            break;
+           }        
+       }
+         return simboloUltimaCarta;
+   }
+   
+   public boolean ultimaCartaEspecial(Carta[] baraja){
+        boolean especialUltimaCarta = true;
+        for(int i = 0; i < baraja.length; i++){
+            if(baraja[i] == null){
+            especialUltimaCarta = baraja[i-1].getEspecial();
+            break;
+           }        
+       }
+         return especialUltimaCarta;
+   }
 }
